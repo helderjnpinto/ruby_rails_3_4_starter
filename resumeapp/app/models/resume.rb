5d13ca82300bb0d023f9da6a06bfdd81e1b4c59d
hjnp <hp_ctt@hotmail.com>
2018-06-06 22:00:43 +0100

    add callbacks to models

--- resumeapp/app/models/resume.rb
+++ resumeapp/app/models/resume.rb
@@ -1,11 +1,16 @@
 class Resume < ActiveRecord::Base
     # attr_accessible :city, :email, :name, :phone, :state, :street, :summary, :zip
+    before_save :do_logging
+
+    def do_logging
+        puts "Saving Object"
+    end
 
     has_many :view_histories
     has_many :jobs
     has_one :cover_letter
     has_and_belongs_to_many :employers
     has_many :applications
-    has_many :employers :through => :applications    
+    has_many :employers, :through => :applications    
 end
 
